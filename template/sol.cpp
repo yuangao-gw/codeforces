49895a383f339d5248b634185cc2d097756d89f2
kalisfa <yuangao.ygao@gmail.com>
2021-12-06 22:27:25 -0500

    foo

--- template/sol.cpp
+++ template/sol.cpp
@@ -2,11 +2,19 @@
 
 using namespace std;
 
+typedef long long ll;
+typedef vecotr<int> vi;
+typedef pair<int, int> pi;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for(int i = a; i <= b; i++)
+
 int main(){
-	ios::sync_with_stdio(false);
+	ios::sync_with_stdio(0);
 	cin.tie(0);
 
 	return 0;
 }
-
-
